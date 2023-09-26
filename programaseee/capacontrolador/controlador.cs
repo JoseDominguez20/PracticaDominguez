5f79577d044299a99ee7df2e534327d583d6dac4
Jose Dominguez <jgominguezg@miumg.edu.gt>
2023-09-25 18:03:43 -0600

    agregar tablas

---
+++ programaseee/capacontrolador/controlador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using capamodelo;
+
+namespace capacontrolador
+{
+    public class controlador
+    {
+        sentencias Senten = new sentencias();
+
+        public datatable Sentnencc 
+        {
+        }
+
+    }
+}
